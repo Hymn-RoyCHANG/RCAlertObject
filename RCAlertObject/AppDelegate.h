ed2018b15934c04096aeaeb422b25bea9ebc2229
Roy CHANG <Roy@roydemacbook-pro.local>
2016-04-29 16:18:59 +0800

    Initial Commit

---
+++ RCAlertObject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RCAlertObject
+//
+//  Created by Roy on 16/4/29.
+//  Copyright © 2016年 Roy CHANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RCAlertObject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RCAlertObject
+//
+//  Created by Roy on 16/4/29.
+//  Copyright © 2016年 Roy CHANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
